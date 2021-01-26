afe241490b6af8bdff9a8e8a3bcbc0b093617fdb
Aryan Srivastava <64788232+aryan-shrivastava09@users.noreply.github.com>
2021-01-26 19:25:45 +0530

    hey

---
+++ day2-start.py
@@ -0,0 +1,12 @@
+#Data Types
+#Strings
+print("Hello"[4])
+print("123"+"345")
+#Integer
+print(123+345)
+print(123_345_567)
+#float
+3.14
+#Boolean
+True
+False
---
+++ ex2.3.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+x = (90 - int(age))*365
+y = int(age)*52
+z = int(age)*12
+print(f"You have {x} days, {y} weeks, and {z} months left.")
