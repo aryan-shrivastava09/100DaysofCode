e8ab23617e2a0c39f01bd439a4b7f1a86f59ffaa
Aryan Srivastava <64788232+aryan-shrivastava09@users.noreply.github.com>
2021-01-30 16:15:34 +0530

    while functions

---
+++ day6-Functions.py
@@ -0,0 +1,36 @@
+#Sum built in functions
+print("Hello")
+len("Hello")
+
+# Creating custom functions
+def my_function():
+  print("hello")
+  print("Bye")
+#Calling the function
+my_function()
+
+# # Reeborg's World 
+# def turn_around():
+#     turn_left()
+#     turn_left()
+# move()
+# move()
+# turn_around()
+# move()
+# move()
+# turn_around()
+
+# def square():
+#     move()
+#     move()
+#     turn_left()
+#     move()
+#     move()
+#     turn_left()
+#     move()
+#     move()
+#     turn_left()
+#     move()
+#     move()
+#     turn_left()
+# square()
---
+++ ex6.1.py
@@ -0,0 +1,28 @@
+# Hurdles race
+# Reeborg has entered a hurdles race. Make him run the course, following the path shown.
+# What you need to know
+# The functions move() and turn_left().
+# Difficulty level
+
+# More advanced
+# You may have noticed that your solution has some repeated patterns. If you know how to define functions, define a function named jump() and use it to simplify your program.
+
+# Difficulty level
+
+# Background image: www.pexels.com
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+for step in range(6):
+    jump()
