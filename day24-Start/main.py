04cadd093c7dc88ed7e02262351750dc80415ff0
Aryan Srivastava <64788232+aryan-shrivastava09@users.noreply.github.com>
2021-02-22 21:15:03 +0530

    snake game update

---
+++ day24-Start/main.py
@@ -0,0 +1,6 @@
+# with open("day24-Start/my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("day24-Start/my_file.txt", mode= "w") as file:
+    file.write("Hello")
--- p20SnakeGame/scoreboard.py
+++ p20SnakeGame/scoreboard.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        # self.high_score = 0
         self.speed(0)
         self.hideturtle()
         self.penup()
@@ -23,3 +24,7 @@ class Scoreboard(Turtle):
         self.goto(0,30)
         self.write("GAME OVER", align="center", font = ("Arial", 20, "bold"))
 
+    # def reset(self):
+    #     if self.score > self.high_score:
+    #         self.high_score = self.score
+    #     self.score = 0
