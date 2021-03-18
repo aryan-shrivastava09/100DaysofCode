823708db60f834f55025c394003e0818673cb6d8
Aryan Srivastava <64788232+aryan-shrivastava09@users.noreply.github.com>
2021-03-18 14:56:10 +0530

    commit

--- p39FlightClub/main.py
+++ p39FlightClub/main.py
@@ -1,6 +1,13 @@
 from data_manager import DataManager
 from pprint import pprint
+from flight_search import FlightSearch
 
 dm = DataManager()
 sheety_data = dm.data
-pprint(sheety_data)
+for d in sheety_data:
+    if d["iataCode"] == "":
+        f = FlightSearch(d)
+source = input("Enter a iata city code you wanna get flights from ")
+
+
+        
